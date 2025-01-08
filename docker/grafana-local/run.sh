ce1e07938e304dc04a22ee3bab568e124c443525
jkrvivian <jkrvivian@gmail.com>
2025-01-08 17:47:30 +0800

    refactor(docker/grafana-local): Add cleanup to run.sh

--- docker/grafana-local/run.sh
+++ docker/grafana-local/run.sh
@@ -11,5 +11,12 @@ fi
 # Update the docker-compose.yaml file with the correct config file
 sed -i "s|./prometheus.yaml:/etc/.*|./${CONFIG_FILE}:/etc/${CONFIG_FILE}|" docker-compose.yaml
 
+cleanup() {
+  echo "Stopping services..."
+  docker compose down
+}
+
+trap cleanup EXIT
+
 # Run docker-compose with the updated configuration file
-docker-compose up -d
+docker compose up
